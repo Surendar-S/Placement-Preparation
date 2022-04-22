2b3609526a85eba25c295f80a3a9c868f586e131
HariPrasath.S.S <71148363+hariprasath6112001@users.noreply.github.com>
2022-04-22 11:39:06 +0530

    String Reverse.c

---
+++ String Reverse.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+    char str[100];
+    scanf("%[^\n]s", str);
+    int i, j, len, pos = 0;
+    for(len=0;str[len]!='\0';len++);
+    for(i=len;i>=0;i--){
+        printf("%c", str[i]);
+    }
+}
