adc1883ade1b4d1cdd72c8333064b2d196713506
HariPrasath.S.S <71148363+hariprasath6112001@users.noreply.github.com>
2022-04-22 11:50:06 +0530

    Panagram.c

--- Panagram.c
+++ Panagram.c
@@ -2,20 +2,26 @@
 #include<ctype.h>
 void main(){
     int i, len, count = 0;
-    char str[] = "abcdefghijklmnopqrstuvwxyz";
+    char str[50];
+    scanf("%[^\n]s", str);
     for(len=0;str[len]!='\0';len++){
         str[i] = tolower(str[i]);
     }
-    char temp[256];
+    int temp[256] = {0};
     for(i=0;i<len;i++)
         temp[str[i]] = 1;
     for(i=0;i<256;i++){
         if(temp[i] == 1)
             count++;
     }
-    printf("%d", count);
     if(count == 26)
         printf("Panagram");
     else
         printf("Not a Panagram");
 }
+
+// I/P: abcdefghijklmnopqrstuvwxyz
+// O/P: Panagram
+
+// I/P: sample
+// O/P: Not a Panagram
